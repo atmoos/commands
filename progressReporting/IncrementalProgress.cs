1c4c86b978bc96ed2d7a66f20f8aafdfa82efb9f
Thomas Kägi <kaegit@gmail.com>
2021-08-03 23:12:06 +0200

    Have incremental progress operate on types of IEquatable<T> in a non monotonic fashion.

--- progressReporting/IncrementalProgress.cs
+++ progressReporting/IncrementalProgress.cs
@@ -2,70 +2,164 @@ using System;
 
 namespace progressReporting
 {
-    internal delegate TProgress Add<TProgress>(in TProgress left, in TProgress right);
-    internal sealed class IncrementalProgress<TProgress> : IProgress<TProgress>
-        where TProgress : unmanaged, IComparable<TProgress>
+    internal delegate void ReportFunc<T>(in T value);
+    internal delegate T NextFunc<T>(in T current, in T value);
+    internal sealed class IncrementalProgress<T> : IProgress<T>
     {
-        private TProgress _next;
-        private readonly Add<TProgress> _add;
-        private readonly TProgress _increment;
-        private readonly IProgress<TProgress> _progress;
-        public IncrementalProgress(IProgress<TProgress> progress, in TProgress increment, Add<TProgress> add, in TProgress root)
+        private ReportFunc<T> report;
+        internal IncrementalProgress(IProgress<T> progress, Func<T, ReportFunc<T>> createReportFunc)
         {
-            _add = add;
-            _next = root;
-            _progress = progress;
-            _increment = increment;
+            this.report = (in T value) => { this.report = createReportFunc(value); progress.Report(value); };
         }
-        public void Report(TProgress value)
+        public void Report(T value) => this.report(in value);
+    }
+
+    internal sealed class Incremental<T>
+        where T : IEquatable<T>
+    {
+        private T next;
+        private readonly NextFunc<T> computeNext;
+        private readonly IProgress<T> progress;
+
+        public Incremental(T seed, NextFunc<T> math, IProgress<T> progress)
         {
-            var current = _next;
-            while(_next.CompareTo(value) <= 0) {
-                _next = _add(_increment, _next);
+            this.next = seed;
+            this.computeNext = math;
+            this.progress = progress;
+        }
+        public void Report(in T value)
+        {
+            var next = this.computeNext(in this.next, in value);
+            if(next.Equals(this.next)) {
+                return;
             }
-            if(_next.CompareTo(current) > 0) {
-                _progress.Report(value);
-            }
+            this.next = next;
+            this.progress.Report(value);
         }
     }
 
     public static class IncrementalProgressExtensions
     {
-        public static IProgress<Double> Incremental(this IProgress<Double> progress, in Double increment, in Double root = default)
+        public static IProgress<Decimal> Incremental(this IProgress<Decimal> progress, Decimal increment)
         {
-            return new IncrementalProgress<Double>(progress, in increment, (in Double l, in Double r) => l + r, in root);
+            Decimal Next(in Decimal current, in Decimal value)
+            {
+                var steps = Math.Floor((value - current) / increment);
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<Single> Incremental(this IProgress<Single> progress, in Single increment, in Single root = default)
+        public static IProgress<Double> Incremental(this IProgress<Double> progress, Double increment)
         {
-            return new IncrementalProgress<Single>(progress, in increment, (in Single l, in Single r) => l + r, in root);
+            Double Next(in Double current, in Double value)
+            {
+                var steps = Math.Floor((value - current) / increment);
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<Byte> Incremental(this IProgress<Byte> progress, in Byte increment, in Byte root = default)
+        public static IProgress<Single> Incremental(this IProgress<Single> progress, Single increment)
         {
-            return new IncrementalProgress<Byte>(progress, in increment, (in Byte l, in Byte r) => (Byte)(l + r), in root);
+            Single Next(in Single current, in Single value)
+            {
+                var steps = (Single)Math.Floor((value - current) / increment);
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<Int32> Incremental(this IProgress<Int32> progress, in Int32 increment, in Int32 root = default)
+        public static IProgress<Byte> Incremental(this IProgress<Byte> progress, Byte increment)
         {
-            return new IncrementalProgress<Int32>(progress, in increment, (in Int32 l, in Int32 r) => l + r, in root);
+            Byte Next(in Byte current, in Byte value)
+            {
+                var steps = (value - current) / increment;
+                return (Byte)(current + (steps * increment));
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<Int64> Incremental(this IProgress<Int64> progress, in Int64 increment, in Int64 root = default)
+        public static IProgress<Int16> Incremental(this IProgress<Int16> progress, Int16 increment)
         {
-            return new IncrementalProgress<Int64>(progress, in increment, (in Int64 l, in Int64 r) => l + r, in root);
+            Int16 Next(in Int16 current, in Int16 value)
+            {
+                var steps = (value - current) / increment;
+                return (Int16)(current + (steps * increment));
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<UInt64> Incremental(this IProgress<UInt64> progress, in UInt64 increment, in UInt64 root = default)
+        public static IProgress<Int32> Incremental(this IProgress<Int32> progress, Int32 increment)
         {
-            return new IncrementalProgress<UInt64>(progress, in increment, (in UInt64 l, in UInt64 r) => l + r, in root);
+            Int32 Next(in Int32 current, in Int32 value)
+            {
+                var steps = (value - current) / increment;
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<UInt32> Incremental(this IProgress<UInt32> progress, in UInt32 increment, in UInt32 root = default)
+        public static IProgress<Int64> Incremental(this IProgress<Int64> progress, Int64 increment)
         {
-            return new IncrementalProgress<UInt32>(progress, in increment, (in UInt32 l, in UInt32 r) => l + r, in root);
+            Int64 Next(in Int64 current, in Int64 value)
+            {
+                var steps = (value - current) / increment;
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<UInt16> Incremental(this IProgress<UInt16> progress, in UInt16 increment, in UInt16 root = default)
+        public static IProgress<UInt64> Incremental(this IProgress<UInt64> progress, UInt64 increment)
         {
-            return new IncrementalProgress<UInt16>(progress, in increment, (in UInt16 l, in UInt16 r) => (UInt16)(l + r), in root);
+            UInt64 Next(in UInt64 current, in UInt64 value)
+            {
+                var steps = (value - current) / increment;
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
         }
-        public static IProgress<SByte> Incremental(this IProgress<SByte> progress, in SByte increment, in SByte root = default)
+        public static IProgress<UInt32> Incremental(this IProgress<UInt32> progress, UInt32 increment)
         {
-            return new IncrementalProgress<SByte>(progress, in increment, (in SByte l, in SByte r) => (SByte)(l + r), in root);
+            UInt32 Next(in UInt32 current, in UInt32 value)
+            {
+                var steps = (value - current) / increment;
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
+        }
+        public static IProgress<UInt16> Incremental(this IProgress<UInt16> progress, UInt16 increment)
+        {
+            UInt16 Next(in UInt16 current, in UInt16 value)
+            {
+                var steps = (value - current) / increment;
+                return (UInt16)(current + (steps * increment));
+            }
+            return CreateIncremental(progress, Next);
+        }
+        public static IProgress<SByte> Incremental(this IProgress<SByte> progress, SByte increment)
+        {
+            SByte Next(in SByte current, in SByte value)
+            {
+                var steps = (value - current) / increment;
+                return (SByte)(current + (steps * increment));
+            }
+            return CreateIncremental(progress, Next);
+        }
+        public static IProgress<TimeSpan> Incremental(this IProgress<TimeSpan> progress, TimeSpan increment)
+        {
+            TimeSpan Next(in TimeSpan current, in TimeSpan value)
+            {
+                var steps = Math.Floor((value - current) / increment);
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
+        }
+        public static IProgress<DateTime> Incremental(this IProgress<DateTime> progress, TimeSpan increment)
+        {
+            DateTime Next(in DateTime current, in DateTime value)
+            {
+                var steps = Math.Floor((value - current) / increment);
+                return current + (steps * increment);
+            }
+            return CreateIncremental(progress, Next);
+        }
+        private static IProgress<T> CreateIncremental<T>(IProgress<T> progress, NextFunc<T> next) where T : IEquatable<T>
+        {
+            return new IncrementalProgress<T>(progress, seed => new Incremental<T>(seed, next, progress).Report);
         }
     }
 }
--- progressReportingTest/IncrementalProgressTest.cs
+++ progressReportingTest/IncrementalProgressTest.cs
@@ -42,17 +42,6 @@ namespace progressReportingTest
         }
 
         [Fact]
-        public void ValuesSmallerThanRootAreExcludedInProgressReport()
-        {
-            var input = new[] { -19283, -4, -3, -2, 0, 5 };
-            var expected = new[] { 0, 5 };
-
-            var actual = TestInt32(input);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
         public void SuccessiveValuesAreExcludedInProgressReport()
         {
             var input = new[] { 0, 4, 4, 4, 4, 9 };
@@ -64,10 +53,10 @@ namespace progressReportingTest
         }
 
         [Fact]
-        public void ProgressReportIncreasesMonotonically()
+        public void ProgressReportIsOrderInsensitive()
         {
-            var input = new[] { 5, 4, 10, -4, 20 };
-            var expected = new[] { 5, 10, 20 };
+            var input = new[] { 0, 5, 4, 10, -4, 20, 10, 12, 16, 4 };
+            var expected = new[] { 0, 5, 10, -4, 20, 10, 16, 4 };
 
             var actual = TestInt32(input);
 
@@ -78,8 +67,8 @@ namespace progressReportingTest
         public void NeighbouringValuesMayBeSmallerThanIncrementAsLongAsTheyFallIntoNeighbouringBins()
         {
             // Bins: [0 - 4[, [4 - 8[, [8 - 12[
-            var input = new[] { 3, 5, 8 };
-            var expected = new[] { 3, 5, 8 };
+            var input = new[] { 0, 3, 5, 8 };
+            var expected = new[] { 0, 5, 8 };
 
             var actual = TestInt32(input);
 
@@ -90,8 +79,8 @@ namespace progressReportingTest
         public void OnlyOneValuePerBinIsReported()
         {
             // Bins: [0 - 4[, [4 - 8[, [8 - 12[
-            var input = new[] { 1, 2, 4, 5, 6, 9, 11 };
-            var expected = new[] { 1, 4, 9 };
+            var input = new[] { 0, 1, 2, 4, 5, 6, 9, 11 };
+            var expected = new[] { 0, 4, 9 };
 
             var actual = TestInt32(input);
 
@@ -112,8 +101,8 @@ namespace progressReportingTest
         [Fact]
         public void WorksWithDoubles()
         {
-            var increment = 0.25;
-            var input = new[] { -3, -Double.Epsilon, 0.5, 1.9, 2.2, 2.3, 2.4, 60, 30, 60.3 };
+            const Double increment = 0.25;
+            var input = new[] { 0.5, 0.55, 1.9, 2.2, 2.3, 2.4, 60, 60.3 };
             var expected = new[] { 0.5, 1.9, 2.2, 2.3, 60, 60.3 };
 
             var actual = TestDouble(increment, input);
@@ -124,17 +113,30 @@ namespace progressReportingTest
         [Fact]
         public void WorksWithUInt16()
         {
-            UInt16 increment = 32;
-            var input = new UInt16[] { 3, 8, 32, 34, 50, 70 };
-            var expected = new UInt16[] { 3, 32, 70 };
+            const UInt16 increment = 32;
+            var input = new UInt16[] { 0, 3, 8, 32, 34, 50, 70, 4 };
+            var expected = new UInt16[] { 0, 32, 70, 4 };
 
             var actual = TestUInt16(increment, input);
 
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void WorksWithTimeSpan()
+        {
+            TimeSpan increment = TimeSpan.FromSeconds(32);
+            var input = new[] { 0, 3, 8, 32, 34, 50, 70, 4 }.Select(f => TimeSpan.FromSeconds(f));
+            var expected = new[] { 0, 32, 70, 4 }.Select(f => TimeSpan.FromSeconds(f));
+
+            var actual = TestTimeSpan(increment, input);
+
+            Assert.Equal(expected, actual);
+        }
+
         private static IEnumerable<Int32> TestInt32(IEnumerable<Int32> input) => Test(p => p.Incremental(4), input);
         private static IEnumerable<UInt16> TestUInt16(UInt16 increment, IEnumerable<UInt16> input) => Test(p => p.Incremental(increment), input);
+        private static IEnumerable<TimeSpan> TestTimeSpan(TimeSpan increment, IEnumerable<TimeSpan> input) => Test(p => p.Incremental(increment), input);
         private static IEnumerable<Double> TestDouble(Double increment, IEnumerable<Double> input) => Test(p => p.Incremental(increment), input);
         private static IEnumerable<TProgress> Test<TProgress>(Func<IProgress<TProgress>, IProgress<TProgress>> getIncrementalProgress, IEnumerable<TProgress> input)
         {
